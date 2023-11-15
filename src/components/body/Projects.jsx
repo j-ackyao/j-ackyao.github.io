1ba0afc2d9870dc19ec1145588f3613da2799ae9
j-ackyao <jackyao2004@gmail.com>
2023-11-15 14:07:36 -0800

    working projects transition

--- src/components/body/Projects.jsx
+++ src/components/body/Projects.jsx
@@ -1,20 +1,72 @@
 import './projects.css'
-import {Parallax, ParallaxLayer} from '@react-spring/parallax';
+// import {Parallax, ParallaxLayer} from '@react-spring/parallax';
+import {useState} from 'react';
+import ImageText from './ImageText';
+import {useTransition, animated} from '@react-spring/web';
 
+
+// function Project() {
+//     return (
+//         <div>project</div>
+//     )
+// }
+
+const params = {
+    subtitle: "Project 1",
+    text: "About 1",
+    src: "https://t4.ftcdn.net/jpg/00/37/56/49/360_F_37564907_UF78p26mRJWYrYqJ97G2Ym5iN4IhDCMX.jpg",
+    reverse: Math.random() > 0.5,
+}
+
+const params2 = {
+    subtitle: "Project 2",
+    text: "About 2",
+    src: "https://t4.ftcdn.net/jpg/00/37/56/49/360_F_37564907_UF78p26mRJWYrYqJ97G2Ym5iN4IhDCMX.jpg",
+    reverse: Math.random() > 0.5,
+}
+
+
+const params3 = {
+    subtitle: "Project 3",
+    text: "About 3",
+    src: "https://t4.ftcdn.net/jpg/00/37/56/49/360_F_37564907_UF78p26mRJWYrYqJ97G2Ym5iN4IhDCMX.jpg",
+    reverse: Math.random() > 0.5,
+}
+
+const projectsList = [params, params2, params3]
 
 export default function Projects() {
-    var index = 0;
+    const [index, setIndex] = useState(0);
+
+    const transition = useTransition(index, {
+        from:  {x: -1500, y: 0, opacity: 0},
+        enter: {x: 0,     y: 0, opacity: 1},
+        leave: {x: 1500,  y: 0, opacity: 0},
+    });
+
+    const next = () => {
+        index >= projectsList.length - 1 ? setIndex(index => 0) : setIndex(index => index + 1);
+    };
+    // const lastIndex = () => {
+    //     index <= 0 ? setIndex(index => projectsList.length - 1) : setIndex(index => index - 1);
+    // }
+
     return (
         <div className='projects-container'>
             <div className='body-subtitle projects'>PROJECTS</div>
-            <Parallax pages={2} onClick={this.scrollTo(1)} className='project-preview' style={{position:'relative', height:'100px'}} horizontal={true}>
-                <ParallaxLayer className='project'>
-                    test
-                </ParallaxLayer>
-                <ParallaxLayer offset={1} className='project'>
-                    test
-                </ParallaxLayer>
-            </Parallax>
+            <button onClick={next}>current index: {index} / {projectsList.length-1}</button>
+            {/* <button onClick={nextVis}>{visible ? 'visible' : 'invisible'}</button> */}
+            <div className='project-display'>
+                {transition((style, item) => {
+                    style.position = 'absolute';
+                    style.transform = 'translateX(-50%)';
+                    return (
+                        <animated.div style={style}> 
+                            <ImageText {...projectsList[item]} />
+                        </animated.div>
+                    );
+                })}
+            </div>
         </div>
-    )
+    );
 }
