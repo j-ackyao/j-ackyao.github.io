6c4c3233c89899559e5dbb5450aa9ce8c1136810
j-ackyao <jackyao2004@gmail.com>
2023-12-25 10:57:31 -0800

    link sometimes doesnt work

--- src/components/body/Projects.jsx
+++ src/components/body/Projects.jsx
@@ -4,6 +4,8 @@ import {useState} from 'react';
 import {ImageTextSized} from './ImageText';
 import {useTransition, animated} from '@react-spring/web';
 
+import tabgpt from '../../resources/tabgpt.png';
+import flappy from '../../resources/flappy.png';
 
 
 const p1 = {
@@ -16,7 +18,7 @@ const p1 = {
 const p2 = {
     subtitle: "Guitar Tab Editor",
     text: "Simple guitar editor with data persistence using JSON and unit testing with JUnit.",
-    src: "https://private-user-images.githubusercontent.com/60021675/292724407-792cfe9e-c079-4041-85f6-649d68c8a20c.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDM0ODgzMzIsIm5iZiI6MTcwMzQ4ODAzMiwicGF0aCI6Ii82MDAyMTY3NS8yOTI3MjQ0MDctNzkyY2ZlOWUtYzA3OS00MDQxLTg1ZjYtNjQ5ZDY4YzhhMjBjLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzEyMjUlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMxMjI1VDA3MDcxMlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWE5MDEzNzA0NjgzZjk3ODhlZTMwNDVlNTMwNzJiNWY1NzczMTljMjI3YTNjMGI5NTY1ZmU5MGQwM2EyZTI1ODcmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.1pe6pVIdJkGpEe_P7utSV09GQWU_fxLtB6lWXonAjfc",
+    src: tabgpt,
     reverse: Math.random() > 0.5,
 }
 
@@ -24,7 +26,7 @@ const p2 = {
 const p3 = {
     subtitle: "KY Engine",
     text: "2D game engine with a simple entity system and axis-aligned bounding box collisions.",
-    src: "https://private-user-images.githubusercontent.com/60021675/292724172-1db94ce7-b6fe-4994-9747-a243b61f7c86.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDM0ODgyODEsIm5iZiI6MTcwMzQ4Nzk4MSwicGF0aCI6Ii82MDAyMTY3NS8yOTI3MjQxNzItMWRiOTRjZTctYjZmZS00OTk0LTk3NDctYTI0M2I2MWY3Yzg2LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzEyMjUlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMxMjI1VDA3MDYyMVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWZjNzY0MmI1YWI4NzE3ODUxNzhlMzdmYjljNDc4NmQzMjhkYWViODk3ZDFkMmI0YWQ2ODAzZGE0YTRhZGJkNDMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.qkwEkzESOoKhuJwghzeloZFOybm8Zcpo9ERK8OhgGPo",
+    src: flappy,
     reverse: Math.random() > 0.5,
 }
 
